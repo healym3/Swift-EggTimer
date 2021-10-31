7fd6991cad97dfcd94ed4f35bd56a410a7262e94
healym3 <79485702+healym3@users.noreply.github.com>
2021-10-30 21:46:29 -0500

    Add alarm sound, Fix timer Times, Update labels for viewer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,26 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
 
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var timerLabel: UILabel!
+    var player: AVAudioPlayer!
+    
     let eggTimes :[String: Int] = ["Soft": 300, "Medium": 420, "Hard": 720]
     
-    var secondsRemaining = 60
+    var secondsPassed = 0
     var timer = Timer()
+    var totalTime = 0
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        progressBar.transform = progressBar.transform.scaledBy(x: 1, y: 10)
         timer.invalidate()
         let hardness = sender.currentTitle!
-        secondsRemaining = eggTimes[hardness]!
-        
+        timerLabel.text = hardness + " Timer Running..."
+        totalTime = eggTimes[hardness]!
+        secondsPassed = 0
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     @objc func updateTimer(){
-        if (secondsRemaining > 0){
-            print("\(secondsRemaining) seconds.")
-            secondsRemaining -= 1
-        }
+        if (secondsPassed < totalTime){
+            let percentageProgressed = Float(secondsPassed)/Float(totalTime)
+            progressBar.progress = percentageProgressed
+            print("\(secondsPassed) seconds.")
+            secondsPassed += 1
+        } else {
+            progressBar.progress=1.0
+            timer.invalidate()
+            timerLabel.text = "Done!"
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+                    player = try! AVAudioPlayer(contentsOf: url!)
+                    player.play()        }
     }
 }
